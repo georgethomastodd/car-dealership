71b4b199cb32795b74aa6968d460712e7f379d35
jacobmikehyde <tomaslgravel@gmail.com>
2022-08-05 11:19:13 -0700

    fix merge conflicts

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -10,6 +10,10 @@ function Nav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+            <li className="nav-item">
+              <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+            </li>
             <li className="nav-item">
                 <NavLink className="nav-link" to="/appts/">Appointments</NavLink>
             </li>
@@ -40,27 +44,18 @@ function Nav() {
             <li className="nav-item">
                 <NavLink className="nav-link" to="/autos/new/">Add Automobile</NavLink>
             </li>
-
-            <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
-            </li>
-
             <li className="nav-item">
               <NavLink className="nav-link" to="/customers/new/">Create New Customer</NavLink>
             </li>
-
             <li className="nav-item">
               <NavLink className="nav-link" to="/salesreps/new/">Create New Sales Rep</NavLink>
             </li>
-
             <li className="nav-item">
               <NavLink className="nav-link" to="/salesrecord/new/">Create New Sale Record</NavLink>
             </li>
-
             <li className="nav-item">
               <NavLink className="nav-link" to="/salesrecord/list/">List all Sales</NavLink>
             </li>
-
             <li className="nav-item">
               <NavLink className="nav-link" to="/salesreps/history/">Sales History</NavLink>
             </li>
--- sales/api/sales_project/settings.py
+++ sales/api/sales_project/settings.py
@@ -9,10 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-<<<<<<< HEAD
-=======
-
->>>>>>> sales-branch
 import dj_database_url
 from pathlib import Path
 
